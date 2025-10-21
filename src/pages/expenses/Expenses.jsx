30f309acc3c7a95048b3ee533c28e2fa7ac0fb3e
plhagyal <pema.lhagyal.work@gmail.com>
2025-10-21 15:57:31 +1100

    October 21, 2025 3:57 PM

--- src/pages/expenses/Expenses.jsx
+++ src/pages/expenses/Expenses.jsx
@@ -654,6 +654,7 @@ const Expenses = () => {
         <div className="modal-actions">
           <button 
             onClick={() => {
+                
               setIsSheetModalOpen(false);
               setNewSheetName('');
             }} 
