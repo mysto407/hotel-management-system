3dae22291b3d61d7c5144e0cd03562385dad5739
plhagyal <pema.lhagyal.work@gmail.com>
2025-10-24 15:07:29 +1100

    October 24, 2025 3:07 PM

--- src/pages/reservations/Reservations.jsx
+++ src/pages/reservations/Reservations.jsx
@@ -1113,7 +1113,7 @@ const Reservations = () => {
             background: color 
           }} />
           <span style={{ fontWeight: '500' }}>{label}:</span>
-          <span style={{ fontWeight: '600', color: '#991b1b' }}>
+          <span style={{ fontWeight: '600', color: '#aaaa' }}>
             {guestCount} {guestCount === 1 ? 'guest' : 'guests'}
           </span>
         </div>
