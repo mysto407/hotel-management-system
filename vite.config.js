231eb47cfbaf4b551d7c2387fcb0a2ccce03d146
plhagyal <pema.lhagyal.work@gmail.com>
2025-11-07 20:36:51 +1100

    tailwind 4

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import tailwindcss from '@tailwindcss/vite' // REMOVE THIS LINE
 import path from 'path'
 
 export default defineConfig({
     plugins: [
         react(),
-        tailwindcss(),
+        // tailwindcss(), // REMOVE THIS LINE
     ],
     resolve: {
         alias: {
